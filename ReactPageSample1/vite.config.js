03a91761804b1d0ef3acb3e0af769eae104469fb
winozz <godwinllabres@gmail.com>
2025-09-06 21:45:54 +0800

    add vte config

--- ReactPageSample1/vite.config.js
+++ ReactPageSample1/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/LabActivity/', // Use your repo name here
   plugins: [react()],
 });
